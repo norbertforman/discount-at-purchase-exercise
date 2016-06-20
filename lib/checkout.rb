cff50d203b9c6efb0544cb533c3ea58e238c2600
Norbert Forman <norbert@Norberts-MacBook-Pro-2.local>
2016-06-20 19:48:39 +0300

    implemented the checkout functionality

---
+++ lib/checkout.rb
@@ -0,0 +1,18 @@
+class Checkout
+  def initialize(promotional_rules)
+    @promotional_rules = promotional_rules
+    @items = []
+  end
+
+  def scan(item)
+    item.validate!
+    @items << item
+  end
+
+  def total
+    @promotional_rules.each do |promotional_rules|
+      promotional_rules.apply!(@items)
+    end
+    return @items.map(&:price).inject(:+).round(2)
+  end
+end
